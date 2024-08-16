ec7778b71578b3e908d8c68ab444339f6bd55787
mhtrkn <mhturknn@gmail.com>
2024-08-16 18:38:51 +0300

    firebase .env setup

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-apiKey: "AIzaSyDl_heCelOuNWZu5b1sk2rJ5BufdhPt24o",
-authDomain: "t-movie-app.firebaseapp.com",
-projectId: "t-movie-app",
-storageBucket: "t-movie-app.appspot.com",
-messagingSenderId: "407220742016",
-appId: "1:407220742016:web:0a0447b22217dd89d7c365"
+apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
